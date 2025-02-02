52e16eb87b4114fff41eb180bf6efdddb9a63c5c
Nikita Karpuk <bitxon.dev@gmail.com>
2025-02-02 16:36:43 +0100

    Base setup

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "kotlin-spring-clouds"
